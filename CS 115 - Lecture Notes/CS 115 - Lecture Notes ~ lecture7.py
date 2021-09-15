f257595f2460e29caec735bcaa9cecd2eab78574
sid_the_coder <siddharth.bhatia555@gmail.com>
2021-09-15 11:14:17 -0400

    Add lecture 7 notes

---
+++ CS 115 - Lecture Notes/CS 115 - Lecture Notes ~ lecture7.py
@@ -0,0 +1,18 @@
+# CS 115-C
+# Sid Bhatia
+# Lecture #7 - Notes ~ Programming with Lists
+
+# Programming with Lists:
+## Indexing
+## Slicing
+## Updating a List
+## Testing for Emptiness
+## len()
+## List Arithmetic
+## range()
+### list()
+## filter()
+## map()
+## functions.reduce
+
+lst = [1,2,3,4] # define a list using bracket '[]' syntax
