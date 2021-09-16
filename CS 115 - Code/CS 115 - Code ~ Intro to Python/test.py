69072c5bcc8ed97bb961e369468732f2df58da22
sid_the_coder <siddharth.bhatia555@gmail.com>
2021-09-15 20:09:59 -0400

    Add folders and files

---
+++ CS 115 - Code/CS 115 - Code ~ Intro to Python/test.py
@@ -0,0 +1,11 @@
+# Sid Bhatia
+# CS 115-C
+# Lecture 4
+# Intro to Python
+
+def solve_zero(): # known as an indentifier
+    return 4-4+4-4 # body of function
+
+print(solve_zero())
+
+print("Hello world")
