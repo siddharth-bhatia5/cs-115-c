c666f7a4cd357531869c1b3b989bb6584a631a80
sid_the_coder <siddharth.bhatia555@gmail.com>
2021-10-12 11:43:08 -0400

    Add lecture 17 and 18 notes - binary representation

---
+++ CS 115 - Lecture Notes/CS 115 - Lecture Notes ~ lecture17 - Binary Representation P2.py
@@ -0,0 +1,15 @@
+# Sid Bhatia
+# CS 115-C
+# Lecture 17 - Binary Representation P2
+
+# Midterm on Friday, 10/15/21
+
+# 4 to 5 questions on Canvas
+
+# Mostly map and reduce
+# 40-45 minutes
+# Closed book but can bring a cheat sheet on notes
+# Cheat sheet has to be handwritten, one-side (8.5 x 11)
+# The other side will be your pledge
+
+# Midterm is similar to quizzes except you have 5 of them
---
+++ CS 115 - Lecture Notes/CS 115 - Lecture Notes ~ lecture18 - Binary Representation P3.py
@@ -0,0 +1,77 @@
+# Sid Bhatia
+# CS 115-C
+# Lecture #18 - Binary Representation P3
+
+# Python Basics
+# Map and Reduce; Lambda Expression; Filter
+# Recursion w/Use-It or Lose-it
+# Binary question --> conversion, addition, and multiplication could be on exam
+# Negative numbers won't be on it
+
+# Closed book and closed notes but have 1 study sheet
+# It'll have the pledge --> PDF
+
+# ----------------------------------------------------
+
+# Converting Between Bases
+
+# 1101_2 --> Base 10
+# 1 = included; 0 = not included
+
+# 1101_2 --> 8 + 4 + 0 + 1 = 13
+
+# 25_10 --> Base 2
+# Half it multiple times and drop fractional part
+
+# Base 2 Multiplication
+
+#  111 x 101
+
+#       1 1 1
+#     x 1 0 1
+#     -------
+#   (1)(1)1 1 1
+#   0 0 0
+#   1 1 1
+#   ---------
+#  1 0 0 0 1 1 = 32 + 2 + 1 = 35
+#   even sum = 0; odd sum = 1
+
+
+# Multiplication w/Russian Peasants:
+# Keep halving (integer dividing) the first number and doubling the second number; add the #s that correspond to the 'odd' first number term
+
+# 21 x 6:
+
+# 21 6 --> selected
+# 10 12
+# 5  24 --> selected
+# 2  48
+# 1  96 --> selected
+
+# therefore, 21 x 6 = 6 + 24 + 96 = 126
+
+# 33 x 7
+
+# 33 7 --> selected
+# 16 14
+# 8  28
+# 4  56
+# 2  112
+# 1  224 --> selected
+
+# therefore, 33 x 7 = 7 + 224 = 231
+
+#           111
+#    x   100001
+#    ----------
+#           111
+#          000
+#         000
+#        000
+#       000
+#      111
+#   -----------
+#      11100111
+
+# Binary subtraction wouldn't be on the exam
