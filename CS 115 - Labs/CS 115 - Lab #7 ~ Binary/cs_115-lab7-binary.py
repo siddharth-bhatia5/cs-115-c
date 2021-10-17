3889c7d0703e32d8876fbab914d3ed67bb967a00
sid_the_coder <siddharth.bhatia555@gmail.com>
2021-10-17 17:04:43 -0400

    Add lab 7 test cases

---
+++ CS 115 - Labs/CS 115 - Lab #7 ~ Binary/cs_115-lab7-binary.py
@@ -0,0 +1,41 @@
+####################################################################################
+# Name: 
+# Pledge: 
+####################################################################################
+
+
+# The binary format you'll be working with for this assignment is called R2L,
+# as it is a right-to-left representation.
+####################################################################################
+## Ex: 8 in decimal is 1000 in standard binary (2^3),
+## and represented as a list [0, 0, 0, 1] in our R2L representation.
+####################################################################################
+
+# Notice that this makes it very easy to work with binary,
+# by using num[0] to grab the least significant bit (2^0)!
+#
+# Please fill out the following 4 functions below using recursion, as specified.
+
+# Given an integer x >= 0, convert it to the R2L binary format.
+# Take note that both [] and [0] are used to represent 0 in R2L
+def decimalToBinary(x):
+    pass
+
+# Given an R2L formatted number, return the integer it is equivalent to.
+# The function should function with both [] and [0] returning 0.
+def binaryToDecimal(num):
+   pass
+
+# Given an R2L formatted number, return an R2L equivalent to num + 1
+# If you need to increase the length, do so. Again, watch out for 0
+def incrementBinary(num):
+   pass
+
+# Given 2 R2L numbers, return their sum.
+## You MUST implement recursively the algorithm for bit-by-bit addition as taught in class,
+## you may NOT do something like decimalToBinary( binaryToDecimal(num1) + binaryToDecimal(num2) ).
+# Make sure to figure out what to do when num1 and num2 aren't of the same length!
+# (and be sure you can add [] and [0])
+## Tip: Try this on paper before typing it up
+def addBinary(num1, num2):
+   pass
